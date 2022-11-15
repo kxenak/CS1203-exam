3f948ec15b5772e3883f2b0027d0617dfeb26d9c
kxenak <kxenak@riseup.net>
2022-11-15 19:36:35 +0000

    Added question 1(a) and 1(b).

--- 1-a.c
+++ 1-a.c
@@ -3,14 +3,20 @@
 
 void basicFizzBuzz(int x)
 {
-    if(x % 33 == 0 || x % 39 == 0 || x % 55 == 0 || x % 65 == 0)
-        printf("fizzbuzz\n");
-    else if(x % 11 == 0 || x % 13 == 0)
-        printf("buzz\n");
-    else if(x % 3 == 0 || x % 5 == 0)
-        printf("fizz\n");
-    else
+    int check = 0;
+    if(x % 3 == 0 || x % 5 == 0)
+    {
+        printf("fizz");
+        check = 1;
+    }
+    if(x % 11 == 0 || x % 13 == 0)
+    {
+        printf("buzz");
+        check = 1;
+    }
+    if(check == 0)
         printf("%d", x);
+    printf("\n");
 }
 
 int main()
--- 1-b.c
+++ 1-b.c
@@ -4,16 +4,16 @@
 void advancedFizzBuzz(int *a, int m, int *b, int nb, int *f, int nf)
 {
     for(int i = 0; i < m; i++)
+    {
+        printf("%d: ", a[i]);
         for(int j = 0; j < nf; j++)
-            for(int k = 0; k < nb; k++)
-                if(a[i] % f[j] == 0 && a[i] % b[k] == 0)
-                    printf("%d: fizzbuzz\n", a[i]);
-                else if(a[i] % b[k] == 0)
-                    printf("%d: buzz\n", a[i]);
-                else if(a[i] % f[j] == 0)
-                    printf("%d: fizz\n", a[i]);
-                else
-                    printf("%d: NA\n", a[i]);
+            if(a[i] % f[j] == 0)
+                printf("fizz");
+        for(int j = 0; j < nb; j++)
+            if(a[i] % b[j] == 0)
+                printf("buzz");
+        printf("\n");
+    }
 }
 
 int main()
